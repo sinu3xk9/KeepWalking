18cbd25a1900b7dc7a269d2484c64e10f8a95af2
Parker Hoot <61476923+ParkerHoot@users.noreply.github.com>
2024-04-24 23:02:59 -0700

    Parker Implemnts UI for the third time
    Imported asset package from old build to re construct the pause menu and main menu because they got reverted

--- Assets/Scripts/Triggers/EventTriggerScript.cs
+++ Assets/Scripts/Triggers/EventTriggerScript.cs
@@ -7,16 +7,20 @@ using UnityEngine.UIElements;
 public class EventTriggerScript : MonoBehaviour
 {
     public List<Animator> eventAnimators;
+    private bool eventPlayed = false;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !eventPlayed)
         {
+            eventPlayed = true;
             foreach(Animator animator in eventAnimators)
             {
                 animator.SetTrigger("EventTrigger");
+                if(this.name.Equals("TrashcanTriggerBox")) {
+                    AudioManager.instance.PlayOneShot(FMODEvents.instance.trashCanFall, this.transform.position);
+                }
             }
-            AudioManager.instance.PlayOneShot(FMODEvents.instance.trashCanFall, this.transform.position);
         }
     }
 }
