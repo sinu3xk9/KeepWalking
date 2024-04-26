1149758639e89b86f99fe10877e7540de62b62ae
Orion Neal <tribe.neal@gmail.com>
2024-04-26 15:33:38 -0600

    event trigger changes
    made audio scalable and added comments for ease of use

--- Assets/Scripts/Triggers/EventTriggerScript.cs
+++ Assets/Scripts/Triggers/EventTriggerScript.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMODUnity;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -7,6 +9,8 @@ using UnityEngine.UIElements;
 public class EventTriggerScript : MonoBehaviour
 {
     public List<Animator> eventAnimators;
+    public String comment;
+    public EventReference eventAudio;
     private bool eventPlayed = false;
 
     void OnTriggerEnter(Collider other)
@@ -14,12 +18,10 @@ public class EventTriggerScript : MonoBehaviour
         if (other.gameObject.CompareTag("Player") && !eventPlayed)
         {
             eventPlayed = true;
+            AudioManager.instance.PlayOneShot(eventAudio, this.transform.position);
             foreach(Animator animator in eventAnimators)
             {
                 animator.SetTrigger("EventTrigger");
-                if(this.name.Equals("TrashcanTriggerBox")) {
-                    AudioManager.instance.PlayOneShot(FMODEvents.instance.trashCanFall, this.transform.position);
-                }
             }
         }
     }
